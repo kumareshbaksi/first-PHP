8ebcf1b4c05e91e137a4e54fbb9687af7e71812d
kumareshbaksi <baksikumaresh@gmail.com>
2023-01-18 16:20:13 +0530

    my first php commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+
+        // Keywords are not case sensitive
+        // Dont forget to add semicolon
+        echo "<h1>Hello World!</h1>";
+        echo "<h1>Hello World!</h1>";
+        echo "<h1>Hello World!</h1>";
+
+        $sallary = 2000;
+        $color = "black";
+
+        echo "my sallary is $sallary";
+        echo "my dog color is $color <br>";
+
+        echo "$sallary, $color <br>";
+        print "$sallary, $color<br>";
+        
+        $float = 6.7;
+
+        echo "$float"
+
+    ?>
+</body>
+</html>
